822b59842fc95a1148622c1a0401baee592ecad0
Justin Swanson <justin.c.swanson@gmail.com>
2017-01-25 22:39:09 -0600

    Switched harmonize config to json.  Refactored branch checkout to common function

--- commoncode.py
+++ commoncode.py
@@ -1,4 +1,6 @@
 import os
+import json
+from git import Repo
 
 class ConfigItem:
 	Nickname = ""
@@ -9,5 +11,23 @@ def ImportHarmonizeConfig():
 		file = open('.harmonize', 'w+')
 		return ConfigItem()
 	
-#	with open('inputfile.txt') as inputfile:
-#		for line in inputfile:
+	ret = []
+	
+	with open('.harmonize') as data_file:
+		data = json.load(data_file)
+		repos = data["Parent-Repos"]
+		for repo in repos:
+			item = ConfigItem()
+			item.Nickname = repo["Nickname"]
+			item.Path = repo["Path"]
+			ret.append(item)
+	return ret
+	
+def CheckoutBranchAtCommit(sha):
+	repo = Repo()
+	assert not repo.bare
+	for b in repo.branches:
+		if b.commit.hexsha == sha:
+			print("Checking out", b)
+			b.checkout()
+			return
--- post-checkout.py
+++ post-checkout.py
@@ -1,19 +1,12 @@
 import sys
-import git
-from commoncode import ImportHarmonizeConfig, ConfigItem
 from git import Repo
+from commoncode import ImportHarmonizeConfig, ConfigItem, CheckoutBranchAtCommit
 
-print ("Test", sys.argv)
 if sys.argv[1] == sys.argv[2]:
 	sys.exit(0)
  
 config = ImportHarmonizeConfig()
- 
-repo = Repo()
-assert not repo.bare
-for b in repo.branches:
-	if b.commit.hexsha == "dfb3ee04c02d797368d8b140a498ef2dacadc263":
-		print("Checking out", b)
-		b.checkout() 
+
+CheckoutBranchAtCommit("dfb3ee04c02d797368d8b140a498ef2dacadc263")
 	
 sys.exit(1)
