c4b7a24b00a8aa2f9d19d42099e3a18b0ab6d6d5
Justin Swanson <justin.c.swanson@gmail.com>
2017-01-27 18:31:19 -0600

    Working post checkout hook that blocks if a parent repo has uncommitted changes

--- commoncode.py
+++ commoncode.py
@@ -5,14 +5,22 @@ from git import Repo
 class ConfigItem:
 	Nickname = ""
 	Path = ""
+	
+class GetResponse:
+	Success = False
+	StatusNote = ""
+	def __init__(self, success, note):
+		self.Success = success
+		self.StatusNote = note
 
 def ImportHarmonizeConfig():
 	if not os.path.exists(".harmonize"):
+		print("No harmonize config existed. Creating empty one.")
 		file = open('.harmonize', 'w+')
 		return ConfigItem()
 	
 	ret = []
-	
+	print("Loading harmonize config")
 	with open('.harmonize') as data_file:
 		data = json.load(data_file)
 		repos = data["Parent-Repos"]
@@ -21,6 +29,7 @@ def ImportHarmonizeConfig():
 			item.Nickname = repo["Nickname"]
 			item.Path = repo["Path"]
 			ret.append(item)
+	print("Found", len(ret), "config items.")
 	return ret
 	
 def CheckoutBranchAtCommit(sha):
@@ -31,3 +40,31 @@ def CheckoutBranchAtCommit(sha):
 			print("Checking out", b)
 			b.checkout()
 			return
+			
+def CheckForUncommitedChanges(repoPath):
+	repo = Repo(path = repoPath)
+	if repo.bare:
+		return GetResponse(True, "Repo was bare.")
+	
+	if repo.is_dirty(untracked_files=True):
+		return GetResponse(True, "Repo had uncommitted changes.")
+	else:
+		return GetResponse(False, "")
+
+def GetLockPath(lockName):
+	return ".git/harmonize-lock-" + lockName
+	
+def Lock(lockName):
+	lockPath = GetLockPath(lockName)
+	if HasLock(lockName):
+		return False
+	file = open(lockPath, 'w+')
+	return True
+	
+def HasLock(lockName):
+	if os.path.isfile(GetLockPath(lockName)):
+		return True
+	return False
+	
+def Unlock(lockName):
+	os.remove(GetLockPath(lockName))
--- post-checkout.py
+++ post-checkout.py
@@ -1,12 +1,26 @@
 import sys
 from git import Repo
-from commoncode import ImportHarmonizeConfig, ConfigItem, CheckoutBranchAtCommit
+from commoncode import *
 
+#If moving to the same commit, just exit
 if sys.argv[1] == sys.argv[2]:
 	sys.exit(0)
+	
+if not Lock("checkout"):
+	print("Breaking out of harmonize checkout logic, as lock could not be achieved.")
+	Unlock("checkout")
+	sys.exit(1)
  
 config = ImportHarmonizeConfig()
 
-CheckoutBranchAtCommit("dfb3ee04c02d797368d8b140a498ef2dacadc263")
-	
-sys.exit(1)
+for parentRepo in config:
+	print("Checking", parentRepo.Nickname, "for changes. (", parentRepo.Path, ")")
+	check = CheckForUncommitedChanges(parentRepo.Path)
+	if check.Success:
+		print(parentRepo.Nickname, "had uncommitted changes.  Reversing checkout.")
+		CheckoutBranchAtCommit(sys.argv[1])
+		sys.exit(1)
+
+print(parentRepo.Nickname, "had no uncommitted changes.")
+Unlock("checkout")
+sys.exit(0)
