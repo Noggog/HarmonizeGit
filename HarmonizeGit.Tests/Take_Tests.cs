4f407cec57fbdcb96491fb56cc3c172df61a8ea8
Justin Swanson <justin.c.swanson@gmail.com>
2017-04-03 19:35:01 -0500

    Take tests

---
+++ HarmonizeGit.Tests/Take_Tests.cs
@@ -0,0 +1,33 @@
+﻿using FishingWithGit;
+using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HarmonizeGit.Tests
+{
+    public class Take_Tests
+    {
+        [Fact]
+        public async Task SyncParentRepos()
+        {
+            using (var checkout = Repository_Tools.GetStandardConfigCheckout())
+            {
+                await checkout.Init();
+                var parent2ndCommit = checkout.ParentRepo.Repo.Lookup<Commit>(checkout.Parent_SecondSha);
+                checkout.ChildToParentListing.SetToCommit(parent2ndCommit);
+                File.WriteAllText(Path.Combine(checkout.Repo.Dir.FullName, HarmonizeGitBase.HarmonizeConfigPath), checkout.Harmonize.Config.GetXmlStr());
+
+                TakeArgs args = new TakeArgs();
+                TakeHandler handler = new TakeHandler(checkout.Harmonize);
+                var ret = await handler.Handle(args.ToArray());
+                Assert.True(ret);
+                Assert.Equal(checkout.Parent_SecondSha, checkout.ParentRepo.Repo.Head.Tip.Sha);
+            }
+        }
+    }
+}
