7186757a902bdb2f01b99ec7c5424cdf9eba6cc9
Justin Swanson <justin.c.swanson@gmail.com>
2017-06-30 15:59:31 -0500

    Added basic tester program

---
+++ HarmonizeGit.Tester/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonizeGit.Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    HarmonizeGitBase harmonize = new HarmonizeGitBase(Environment.CurrentDirectory);
+                    await harmonize.Handle(args);
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.ToString());
+                }
+            });
+            System.Console.ReadLine();
+        }
+    }
+}
